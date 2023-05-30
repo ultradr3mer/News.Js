2f791d6bc0bafc345566b95acf961137943eab5a
ultradr3mer <dulz@hotmail.de>
2023-05-30 17:57:51 +0200

    News added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppHeader from './components/layout/AppHeader';
 import SideMenu from './components/layout/SideMenu';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import {Layout} from 'antd';
-const {Content} = Layout;
+import NewsGrid from './components/layout/NewsGrid';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Layout } from 'antd';
+const { Content } = Layout;
 
 class App extends Component {
   render() {
-      return (
-          <Layout>
-              <Router>
-                  <Fragment>
-                      <AppHeader/>
-                      <Content style={{padding: '0 20px'}}>
-                            <Layout style={{padding: '24px 0', background: '#fff'}}>
-                                <SideMenu/>
-                            </Layout>
-                        </Content>
-                  </Fragment>
-              </Router>
-          </Layout>
-
-      );
+    return (
+      <Layout>
+        <Router>
+          <Fragment>
+            <AppHeader />
+            <Content style={{ padding: '0 20px' }}>
+              <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                <SideMenu />
+                <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                  <NewsGrid />
+                </Content>
+              </Layout>
+            </Content>
+          </Fragment>
+        </Router>
+      </Layout>
+    );
   }
 }
 
